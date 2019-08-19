80ff3079b8f5e76abc506766350fbfb1e5c3963f
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-19 20:30:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
-def is_prime?(num)
-  n = 2
-  while n < num
-    return false if num % n == 0
-    n += 1
+def prime?(number)
+(2..(number - 1)).each do |n|
+    return false if number % n == 0
   end
   true
 end
